f6e8cb1e53e2ea9bf61b8edabef5f5a4e3172885
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2025-11-16 14:38:19 +0330

    Fix array input parsing with proper Split and RemoveEmptyEntries

---
+++ LongestIncreasingSubarray/LongestIncreasingSubarray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestIncreasing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter integers separated by space: ");
+            int[] arr = Array.ConvertAll(
+                Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries),
+                int.Parse
+            );
+
+
+            int maxLength = 1;
+            int currentLength = 1;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[i - 1])
+                    currentLength++;
+                else
+                    currentLength = 1;
+
+                if (currentLength > maxLength)
+                    maxLength = currentLength;
+            }
+
+            Console.WriteLine($"Length of longest increasing subarea: {maxLength}");
+        }
+    }
+}
